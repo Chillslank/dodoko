84f91c0c62be50c48adbfdcec31a724d386ddff1
Dingyuan Gao <2600997g@student.gla.ac.uk>
2021-08-06 17:06:03 +0800

    fix bug

--- static/js/home.js
+++ static/js/home.js
@@ -1,5 +1,4 @@
 const arr = document.querySelectorAll('.home-list-group-item')
-console.log(arr)
 for (let i = 0; i < 5; i++) {
   arr[i].style.animationDelay = `${i * 0.2}s`    
   arr[i + 5].style.animationDelay = `${i * 0.2}s`    
